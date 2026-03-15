2d294e5c5c21a8c3de1698c6ff1e5e45d34611b7
c-ed <csedwards79@gmail.com>
2026-03-15 08:50:12 -0700

    migrate bun dev server to Bun.serve

--- orchestrators/bun/src/bun-globals.d.ts
+++ orchestrators/bun/src/bun-globals.d.ts
@@ -3,4 +3,15 @@ declare const Bun: {
     text(): Promise<string>;
   };
   write(destination: string | URL, input: string | Blob): Promise<number>;
+  serve(options: {
+    hostname?: string;
+    idleTimeout?: number;
+    port: number;
+    fetch(request: Request): Response | Promise<Response>;
+  }): {
+    hostname: string;
+    port: number;
+    url: URL;
+    stop(closeActiveConnections?: boolean): void;
+  };
 };
